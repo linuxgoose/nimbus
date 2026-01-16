4da56023e443cb8cae0b07b5def3317273280556
Jordan Robinson <hello@jordanrobinson.org>
2026-01-16 00:39:25 +0000

    remove unused imports

--- lib/app/controller/controller.dart
+++ lib/app/controller/controller.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_timezone/flutter_timezone.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
